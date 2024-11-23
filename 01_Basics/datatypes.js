e80224080fc045abb4b54f8ee122b931c7c9e49f
yashkamde4 <164876672+yashkamde4@users.noreply.github.com>
2024-11-23 14:36:12 +0000

    datatypes

---
+++ 01_Basics/datatypes.js
@@ -0,0 +1,18 @@
+"use strict";
+
+console.log(3 + 3)
+console.log("Yash");
+
+// Data Types
+
+let name = "Yash"   // String
+let age = 21  // int 
+let isLoggedIn = true // boolean true or false
+
+
+let state = null    // There is difference in null & undefined i.e null is empty(but given) and undefined is not given 
+let address;   // (undefined)
+
+console.log(typeof "yash");
+console.log(typeof null);
+
