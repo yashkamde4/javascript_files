fe99c92f6021b3462cbdd139af334f6acb4f4416
yashkamde4 <164876672+yashkamde4@users.noreply.github.com>
2024-11-23 14:25:47 +0000

    variables

---
+++ 01_Basics/variables.js
@@ -0,0 +1,13 @@
+const accountId = 3345
+let accountEmail = "yash@gmail.com"
+var accountPass = "1234"
+accountCity = "Jaipur"
+let accountState;
+
+// console.log(accountId);
+// Note Only prefer to use let instead of var
+
+accountCity = "Nagpur"
+
+console.table([accountId, accountEmail, accountPass, accountCity, accountState]);
+
