2f1b5cb47a5d3a652c7f132cdb95772bf0e75f2a
yashkamde4 <164876672+yashkamde4@users.noreply.github.com>
2024-11-24 16:10:10 +0000

    conversion

---
+++ 01_Basics/conversionOperation.js
@@ -0,0 +1,27 @@
+let score = "Yash"
+
+// console.log(typeof score);
+// console.log(typeof (score))
+
+let valueInNum = Number(score)
+console.log(typeof valueInNum);
+console.log(valueInNum);    
+
+// "33" = 33
+// "33abc" = NaN (Not a Number)
+// true = 1  false = 0
+
+let isloggedIn = "Hitesh"
+let booleanLoggedIn = Boolean(isloggedIn)
+
+console.log(booleanLoggedIn);
+
+// empty = false(0)
+// if not empty = true(1)
+
+let num = 33
+
+let stringNum = String(num)
+console.log(typeof stringNum);
+console.log(stringNum);
+
